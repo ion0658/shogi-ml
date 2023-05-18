c63d891d063139b0d40b60c2bb9fac6fad01f1c4
ion0658 <y0a6s5u8soy@gmail.com>
2023-05-18 11:15:31 +0900

    wip

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,25 @@
-mod board;
-mod game;
-mod piece;
-
 use anyhow::Result;
-use game::Game;
+use shogi_alg::game::*;
 use sqlx::{migrate::MigrateDatabase, Sqlite};
-use std::path::Path;
-
-const TASK_NUMBER: usize = 500;
+use std::{env, path::Path};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let game_number = if args.len() > 1 {
+        args[1].parse::<usize>().unwrap_or(1)
+    } else {
+        1
+    };
+    run(game_number).await?;
+    Ok(())
+}
+
+async fn run(game_number: usize) -> Result<()> {
     let pool = get_connection().await?;
     sqlx::migrate!().run(&pool).await?;
     let mut tasks = vec![];
-    for _ in 0..TASK_NUMBER {
+    for _ in 0..game_number {
         tasks.push(tokio::spawn(game_task(pool.clone())));
     }
     let elapsed_list = futures::future::try_join_all(tasks)
@@ -36,7 +41,7 @@ async fn game_task(pool: sqlx::SqlitePool) -> Result<u128> {
     let start = std::time::Instant::now();
     loop {
         match game.next() {
-            game::GameState::Checkmate(_color) => {
+            GameState::Checkmate(_color) => {
                 // println!("Checkmate! {:?} is Winner!", color);
                 // game.print();
                 break;
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+mod board;
+pub mod game;
+mod piece;
