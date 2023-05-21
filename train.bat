ef8a4a31c1f3444aa79903d0f660819ac59322f0
y0a6s <y0a6s@IONDESK>
2023-05-21 18:26:55 +0900

    wip

--- train.bat
+++ train.bat
@@ -1,10 +1,8 @@
 @echo off
 set GAMES=256
-set START_EPOCK=10
 set EPOCHS=10
-set /a FIN_EPOCH=%START_EPOCK%+EPOCHS
 
-for /l %%i in (%START_EPOCK%, 1, %FIN_EPOCH%) do (
+for /l %%i in (0, 1, %EPOCHS%) do (
     echo "EPOCH %%i"
     cargo run --release --bin train %GAMES%
     python .\py_src\train.py
